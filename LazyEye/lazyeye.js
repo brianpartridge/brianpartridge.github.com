aa3e463485e057d85de4f5713bdce79990892444
Brian Partridge <brianpartridge@gmail.com>
2013-10-01 23:21:29 -0400

    implement simplified redesign
    - Based on http://startbootstrap.com/the-big-picture

--- LazyEye/lazyeye.js
+++
@@ -1,6 +0,0 @@
-javascript:(function(){
-    var l = window.location, p = l.protocol;
-    if (p == 'http:') { l.protocol = 'lazyeye:'; }
-    else if (p == 'https:') { l.protocol = 'lazyeyes:'; }
-    else { alert('Sorry, unsupported URL.'); }
-})();
